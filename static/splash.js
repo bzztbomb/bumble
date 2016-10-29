627c50df4cc920206be818628124569b84417ca1
jason <jason@lucifer.noisybox.net>
2016-10-28 23:08:24 -0700

    splash screen

---
+++ static/splash.js
@@ -0,0 +1,23 @@
+setInterval(changeBackground, 10);
+
+let r = 150;
+let g = 0;
+let b = 150;
+
+function changeBackground(){
+  let which = _.random(0,2);
+  let amt = _.random(-3, 3);
+  switch(which){
+    case 0:
+      r = Math.max(0, Math.min((r + amt), 256));
+      break;
+    case 1:
+      g = Math.max(0, Math.min((g + amt), 256));
+      break;
+    case 2:
+      b = Math.max(0, Math.min((b + amt), 256));
+      break;
+  }
+  let color = `rgb(${r},${g},${b})`;
+  $('body').css('background-color', color);
+}
