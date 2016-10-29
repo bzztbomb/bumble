0473ac8c6dc5ade62c2e746a80a9e9f18d2d4d8d
jason <jason@lucifer.noisybox.net>
2016-10-29 10:59:53 -0700

    promisify image loading

--- static/bumble.js
+++ static/bumble.js
@@ -1,23 +1,30 @@
 
 
-$( document ).ready(function() {
+$( document ).ready(() => {
 
   console.log( "ready!" );
   getImageName()
-    .then(filename => {
-      let img = new Image();
-      img.className = 'imgitem';
-      img.onload = () => {
-        img = $(img);
-        $('body').append(img);
-        console.log(`loaded ${filename} ${img.width()}x${img.height()}`);
+    .then(loadImage)
+    .then(img => {
         img.css({left: -1 * img.width(), top: -1 * img.height()});
         img.velocity({top: $(document).height() + 'px', left: $(document).width() + 'px'},
             {easing: null, duration: 6000, complete: () => console.log('done animating')});
-      }
-      img.src = `/images/${filename}`;
-    })
+      });
 });
+
+function loadImage(filename){
+  return new Promise((fulfill,reject) => {
+    let img = new Image();
+    img.className = 'imgitem';
+    img.onload = () => {
+      img = $(img);
+      $('body').append(img);
+      console.log(`loaded ${filename} ${img.width()}x${img.height()}`);
+      fulfill(img);
+    };
+    img.src = `/images/${filename}`;
+  });
+}
 
 function getImageName(){
   return new Promise((fulfill, reject) => {
