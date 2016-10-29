7dfebb5dcdf72497fd5d476b5712e6e1409b3b84
jason <jason@lucifer.noisybox.net>
2016-10-29 10:54:36 -0700

    things are moving!

--- static/bumble.js
+++ static/bumble.js
@@ -3,13 +3,23 @@
 $( document ).ready(function() {
 
   console.log( "ready!" );
-  getImage()
-    .then(image => {
-      $('#container').append(`<img id="theImg" src="/images/${image}" width="400px;"/>`);
+  getImageName()
+    .then(filename => {
+      let img = new Image();
+      img.className = 'imgitem';
+      img.onload = () => {
+        img = $(img);
+        $('body').append(img);
+        console.log(`loaded ${filename} ${img.width()}x${img.height()}`);
+        img.css({left: -1 * img.width(), top: -1 * img.height()});
+        img.velocity({top: $(document).height() + 'px', left: $(document).width() + 'px'},
+            {easing: null, duration: 6000, complete: () => console.log('done animating')});
+      }
+      img.src = `/images/${filename}`;
     })
 });
 
-function getImage(){
+function getImageName(){
   return new Promise((fulfill, reject) => {
     $.get('/i', file => {
       fulfill(file);
