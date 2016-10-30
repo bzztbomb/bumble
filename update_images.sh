be821a453724e19df5699e7fb63bb5025dac9db6
jason <jason@noisybox.net>
2016-10-30 10:39:55 -0700

    better update script

--- update_images.sh
+++ update_images.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 DIR=`dirname "$0"`
-rsync -avv "${DIR}/original_images/" "${DIR}/static/images/"
+rsync -avv --delete --exclude README.md \
+	"${DIR}/original_images/" "${DIR}/static/images/"
+
+OIFS="$IFS"
+IFS=$'\n'
 for f in `find "${DIR}/static/images/"` ; do
 	echo "$f"
-	mogrify -trim "${f}"
+	mogrify -resize 750x750\> -trim $f
 done
+IFS="$OIFS"
